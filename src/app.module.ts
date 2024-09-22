816130784d7551624aae9cb7d19bf4a161109604
El buen y fresco Kirby <167347481+KevinMondragonDev@users.noreply.github.com>
2024-09-22 17:02:57 -0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      //Este ultimo es para los cambios en las entidades se reflejen en las tablas
+      //No es comum que se use produccion
+      synchronize:true,
+    }),
+    
+    ProductsModule
+  ],
+})
+export class AppModule {}
+
+
+      //synchronize:true, es para sincronizar los cambios con la base de datos
+      //no se usa en produccion
